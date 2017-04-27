36e7fd49b298319e70d7158e4aed6a7f7d065494
omnibot2k <omnibot2k@gmail.com>
2017-04-27 05:01:48 -0400

    WIP-stateless-func-components
    530 in video

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,41 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+class SelectLanguage extends React.Component {
+
+	render() {
+		var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+
+    return (
+	    
+	    //when moving this up here replace state with props
+	    //  state lives in popular component		
+			<ul className='languages'>
+				<p>Selected language: {this.props.selectedLanguage}</p>
+				{ 
+					languages.map(function (lang){
+						
+					//prior to passing the second param this to map this was undefined!!	
+					console.log('Down here', this);
+					return (			
+						<li 
+							style = {lang === this.props.selectedLanguage ? {color : '#d0021b'} : null}
+									onClick={this.props.onSelect.bind(null, lang)}
+							key={lang}>
+								{lang}
+						</li>
+					)
+				}, this)}
+			</ul>	
+		)
+	}
+}
+
+SelectLanguage.propTypes = {
+	selectedLanguage: PropTypes.string.isRequired,
+	onSelect: PropTypes.func.isRequired,
+}
+
 
 class Popular extends React.Component {
 	constructor (props) {
@@ -9,7 +46,7 @@ class Popular extends React.Component {
 			selectedLanguage: 'All'
 		};
 
-        //define a new function this.function that is always bound to this object
+				//define a new function this.function that is always bound to this object
 		this.updateLanguage = this.updateLanguage.bind(this);
 	}
 	updateLanguage(lang) {
@@ -20,28 +57,15 @@ class Popular extends React.Component {
 		});
 	}
 	render() {
-		var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 		console.log('Up here!', this);
 		
 		return (
-		  <ul className='languages'>
-		    <p>Selected language: {this.state.selectedLanguage}</p>
-		    { 
-		      languages.map(function (lang){
-		      
-			    //prior to passing the second param this to map this was undefined!!	
-			    console.log('Down here', this);
-			   	  
-			   	return (
-			   	  <li 
-			   	    style = {lang === this.state.selectedLanguage ? {color : '#d0021b'} : null}
-	                onClick={this.updateLanguage.bind(null, lang)}
-			   	  	key={lang}>
-			   	  	{lang}
-			   	  </li>
-			   	 )
-			  }, this)}
-		  </ul>
+			<div>
+				<SelectLanguage
+					selectedLanguage={this.state.selectedLanguage}
+					onSelect={this.updateLanguage}
+				/>
+			</div>
 		)
 	}
 }
